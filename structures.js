f02581eebb27a32e576e4e216daffddd44078cb9
Thomas McGrath <thomasmcg32@gmail.com>
2021-03-02 00:00:00 -0500

    Add functionality to len and palindrome functions

--- structures.js
+++ structures.js
@@ -42,6 +42,38 @@ keys.addEventListener('click', (event) => {
     reverse(input1.value);
     updateDisplay();
   }
+  if (target.classList.contains('pal-button')) {
+    const input1 = document.getElementById('pal-input');
+    const str = " ";
+
+    updateDisplay();
+    palindrome(input1.value);
+    updateDisplay();
+  }
+  if (target.classList.contains('len-button')) {
+    const input1 = document.getElementById('len-input');
+    const str = " ";
+
+    updateDisplay();
+    len(input1.value);
+    updateDisplay();
+  }
+  if (target.classList.contains('upp-button')) {
+    const input1 = document.getElementById('upp-input');
+    const str = " ";
+
+    updateDisplay();
+    toUpper(input1.value);
+    updateDisplay();
+  }
+  if (target.classList.contains('low-button')) {
+    const input1 = document.getElementById('low-input');
+    const str = " ";
+
+    updateDisplay();
+    toLower(input1.value);
+    updateDisplay();
+  }
   inputDigit(target.value);
   updateDisplay();
 });
@@ -99,6 +131,41 @@ function reverse(string) {
   calculator.displayValue = str;
 }
 
+function palindrome(input) {
+  var beg = 0;
+  var end = input.length - 1;
+  var mid = (beg + end) / 2;
+  
+
+  for(i = 0; i <= mid; i++) {
+    if (input[beg] == input[end]) {
+       beg ++;
+       end --;
+        if (i == (mid)) {
+          calculator.displayValue = "TRUE"
+        }
+       else{
+          calculator.displayValue = "FALSE"
+        }
+     }
+  }
+}
+
+function len(input) {
+  var i = 0;
+  var count = 0;
+  for(i = 0; i < input.length; i++) {
+    count += 1;
+  }
+  calculator.displayValue = count;
+}
+function toUpper(input) {
+  
+}
+function toLower(input) {
+  
+}
+
 
 
 
@@ -116,7 +183,6 @@ function resetCalculator() {
 
 function chooseMethod() {
   const method = this.value;
-
   if(method == "Arithmetic") {
     const butt = '<button class = "is-clear" value = "clear">C</button><button class = "operator" value = "/" >/</button><button class = "calc-button" value = "7">7</button><button class = "calc-button" value = "8">8</button><button class = "calc-button" value = "9">9</button><button class = "operator" value = "x">x</button><button class = "calc-button" value = "4">4</button><button class = "calc-button" value = "5">5</button><button class = "calc-button" value = "6">6</button><button class = "operator" value = "-">-</button><button class = "calc-button" value = "1">1</button><button class = "calc-button" value = "2">2</button><button class = "calc-button" value = "3">3</button><button class = "operator" value = "+">&plus;</button><button class = "is-zero" value = "0">0</button><button class = "operator" value = "=" id = "is-equals">&equals;</button></div>'
     const str = document.querySelector('.bottom')
@@ -124,13 +190,13 @@ function chooseMethod() {
 
   } else if (method == "Strings") {
 
-    const butt = '<input type = "input" class = "is-input" placeholder = "Enter String" id = "rev-input" value = ""></input><button class = "rev-button">Reverse</button></div><input type = "input" class = "is-input" placeholder = "Enter String"></input><button class = "str-button" value = "Palindrome">Palindrome</button><input type = "input" class = "is-input" placeholder = "Enter String"></input><button class = "str-button" value = "length">Length</button><input type = "input" class = "is-input " placeholder = "Enter String"></input><button class = "str-button" value = "upperCase">upperCase</button><input type = "input" class = "is-input " placeholder = "Enter String"></input><button class = "str-button" value = "lowerCase">lowerCase</button></div>'
+    const butt = '<input type = "input" class = "is-input" placeholder = "Enter String" id = "rev-input" value = ""></input><button class = "rev-button">Reverse</button></div><input type = "input" class = "is-input" placeholder = "Enter String" id = "pal-input" value = ""></input><button class = "pal-button" >Palindrome</button><input type = "input" class = "is-input" placeholder = "Enter String" id = "len-input" value = ""></input><button class = "len-button" >Length</button><input type = "input" class = "is-input " placeholder = "Enter String" id = "upp-input" value = ""></input><button class = "upp-button" >upperCase</button><input type = "input" class = "is-input " placeholder = "Enter String" id = "low-input" value = ""></input><button class = "low-button" >lowerCase</button></div>'
     const str = document.querySelector('.bottom')
     str.innerHTML = butt;
   } else if (method == "Sorting") {
     sorting(method);
   } else {
-
+    /* Figure out what to do here */
    }
 }
 
