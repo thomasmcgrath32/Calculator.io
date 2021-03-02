a4bf39982424bff8275ef22c6ad9ebb8ddff0f52
Thomas McGrath <thomasmcg32@gmail.com>
2021-03-02 01:52:17 -0500

    Added functionality and fixed bugs

--- structures.js
+++ structures.js
@@ -8,7 +8,6 @@ function updateDisplay() {
   const display = document.querySelector('.calculator-screen');
   display.value = calculator.displayValue;
 }
-
 updateDisplay();
 
 /* Checking for 'click' on calc-button */
@@ -28,7 +27,6 @@ keys.addEventListener('click', (event) => {
     updateDisplay();
     return;
   }
-
   if (target.classList.contains('is-clear')) {
     resetCalculator();
     updateDisplay();
@@ -36,42 +34,37 @@ keys.addEventListener('click', (event) => {
   }
   if (target.classList.contains('rev-button')) {
     const input1 = document.getElementById('rev-input');
-    const str = " ";
-
-    updateDisplay();
     reverse(input1.value);
     updateDisplay();
   }
   if (target.classList.contains('pal-button')) {
-    const input1 = document.getElementById('pal-input');
-    const str = " ";
-
     updateDisplay();
+    const input1 = document.getElementById('rev-input');
     palindrome(input1.value);
     updateDisplay();
   }
   if (target.classList.contains('len-button')) {
-    const input1 = document.getElementById('len-input');
-    const str = " ";
-
     updateDisplay();
+    const input1 = document.getElementById('rev-input');
     len(input1.value);
     updateDisplay();
   }
-  if (target.classList.contains('upp-button')) {
-    const input1 = document.getElementById('upp-input');
-    const str = " ";
-
+  if (target.classList.contains('cv-button')) {
     updateDisplay();
-    toUpper(input1.value);
+    const input1 = document.getElementById('rec-input');
+    countVowels(input1.value);
     updateDisplay();
   }
-  if (target.classList.contains('low-button')) {
-    const input1 = document.getElementById('low-input');
-    const str = " ";
-
+  if (target.classList.contains('rmv-button')) {
     updateDisplay();
-    toLower(input1.value);
+    const input1 = document.getElementById('rev-input');
+    removeVowels(input1.value);
+    updateDisplay();
+  }
+  if (target.classList.contains('bub-button')) {
+    updateDisplay();
+    const input1 = document.getElementById('rev-input');
+    bubbleSort(input1.value);
     updateDisplay();
   }
   inputDigit(target.value);
@@ -85,7 +78,7 @@ function inputDigit(digit) {
     calculator.displayValue = digit;
     calculator.waitingForSecondOperand = false;
   } else {
-  calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
   }
 }
 
@@ -94,7 +87,6 @@ function handleOperator(nextOperator) {
   const inputValue = parseFloat(displayValue);
   if(operator && calculator.waitingForSecondOperand) {
     calculator.operator = nextOperator;
-
     return;
   }
   if (firstOperand === null && !isNaN(inputValue)) {
@@ -102,11 +94,10 @@ function handleOperator(nextOperator) {
   } else if (operator) {
     const result = calculate(firstOperand,inputValue,operator);
     calculator.displayValue = String(result);
-   calculator.firstOperand = result;
+    calculator.firstOperand = result;
   }
-  calculator.waitingForSecondOperand = true;
- calculator.operator = nextOperator;
-
+   calculator.waitingForSecondOperand = true;
+   calculator.operator = nextOperator;
 }
 
 function calculate(firstOperand, secondOperand, operator) {
@@ -123,8 +114,9 @@ function calculate(firstOperand, secondOperand, operator) {
 }
 
 function reverse(string) {
-  var i,j;
-  var str = "";
+var i;
+var str = "";
+
   for(i = string.length - 1; i >= 0; i--) {
     str += string[i];
   }
@@ -135,19 +127,17 @@ function palindrome(input) {
   var beg = 0;
   var end = input.length - 1;
   var mid = (beg + end) / 2;
-  
 
   for(i = 0; i <= mid; i++) {
     if (input[beg] == input[end]) {
        beg ++;
        end --;
-        if (i == (mid)) {
+        if (beg == end) {
           calculator.displayValue = "TRUE"
         }
-       else{
-          calculator.displayValue = "FALSE"
-        }
-     }
+     }else{
+      calculator.displayValue = "FALSE"
+    }
   }
 }
 
@@ -159,16 +149,87 @@ function len(input) {
   }
   calculator.displayValue = count;
 }
-function toUpper(input) {
-  
+
+function countVowels(input) {
+  var i = 0;
+  var count = 0;
+
+  for(i = 0; i <= input.length -1; i++) {
+    if (input[i] == "a" || input[i] == "e" || input[i] == "i" || input[i] == "o" || input[i] == "u") {
+      count += 1;
+    }else {
+      count += 0;
+    }
+  }
+  calculator.displayValue = count;
 }
-function toLower(input) {
-  
+
+function removeVowels(input) {
+  var i = 0;
+  var count = 0;
+  var str = "";
+  for(i = 0; i <= input.length -1; i++) {
+    if (input[i] == "a" || input[i] == "e" || input[i] == "i" || input[i] == "o" || input[i] == "u") {
+      continue;
+    }else {
+      str += input[i];
+    }
+  }
+  calculator.displayValue = str;
 }
 
 
+function bubbleSort(input) {
+  var i,j,k,l;
+  var x = [];
+  var y = "";
+  str = "";
 
+  for(i = 0; i <= input.length -1; i++) {
+    if (input[i] != ",") {
+      str += input[i];
+    }
+  }
 
+  for(j = 0; j <= str.length -1; j++) {
+    x[j] = str[j];
+  }
+
+  for(k = 0; k <= str.length-1; k++) {
+    for(l = 0; l <= str.length -1; l++) {
+      if(x[l] > x[l + 1]) {
+         y = x[l + 1];
+        x[l+1] = x[l];
+        x[l] = y;
+      }
+    }
+  }
+
+ 
+  calculator.displayValue = x;
+}
+/*
+function insertionSort(input) {
+
+}
+
+function selectionSort(input) {
+
+}
+
+function mergeSort(input) {
+
+}
+
+funciton heapSort(input) {
+
+}
+
+function quickSort() {
+
+}
+
+*/
 
 function resetCalculator() {
   calculator.displayValue = ' ';
@@ -177,31 +238,30 @@ function resetCalculator() {
   calculator.operator = null;
 }
 
-
-
-
-
 function chooseMethod() {
   const method = this.value;
-  if(method == "Arithmetic") {
-    const butt = '<button class = "is-clear" value = "clear">C</button><button class = "operator" value = "/" >/</button><button class = "calc-button" value = "7">7</button><button class = "calc-button" value = "8">8</button><button class = "calc-button" value = "9">9</button><button class = "operator" value = "x">x</button><button class = "calc-button" value = "4">4</button><button class = "calc-button" value = "5">5</button><button class = "calc-button" value = "6">6</button><button class = "operator" value = "-">-</button><button class = "calc-button" value = "1">1</button><button class = "calc-button" value = "2">2</button><button class = "calc-button" value = "3">3</button><button class = "operator" value = "+">&plus;</button><button class = "is-zero" value = "0">0</button><button class = "operator" value = "=" id = "is-equals">&equals;</button></div>'
-    const str = document.querySelector('.bottom')
-    str.innerHTML = butt;
-
+  if(method == "Integers") {
+    resetCalculator();
+    updateDisplay();
+      const butt = '<button class = "is-clear" value = "clear">C</button><button class = "operator" value = "/" >/</button><button class = "calc-button" value = "7">7</button><button class = "calc-button" value = "8">8</button><button class = "calc-button" value = "9">9</button><button class = "operator" value = "x">x</button><button class = "calc-button" value = "4">4</button><button class = "calc-button" value = "5">5</button><button class = "calc-button" value = "6">6</button><button class = "operator" value = "-">-</button><button class = "calc-button" value = "1">1</button><button class = "calc-button" value = "2">2</button><button class = "calc-button" value = "3">3</button><button class = "operator" value = "+">&plus;</button><button class = "is-zero" value = "0">0</button><button class = "operator" value = "=" id = "is-equals">&equals;</button></div>'
+      const str = document.querySelector('.bottom');
+      str.innerHTML = butt;
   } else if (method == "Strings") {
-
-    const butt = '<input type = "input" class = "is-input" placeholder = "Enter String" id = "rev-input" value = ""></input><button class = "rev-button">Reverse</button></div><input type = "input" class = "is-input" placeholder = "Enter String" id = "pal-input" value = ""></input><button class = "pal-button" >Palindrome</button><input type = "input" class = "is-input" placeholder = "Enter String" id = "len-input" value = ""></input><button class = "len-button" >Length</button><input type = "input" class = "is-input " placeholder = "Enter String" id = "upp-input" value = ""></input><button class = "upp-button" >upperCase</button><input type = "input" class = "is-input " placeholder = "Enter String" id = "low-input" value = ""></input><button class = "low-button" >lowerCase</button></div>'
-    const str = document.querySelector('.bottom')
+      resetCalculator();
+      updateDisplay();
+      const butt = '<input type = "input" class = "is-input" placeholder = "Enter String" id = "rev-input" value = ""></input><button class = "rev-button">Reverse</button></div><button class = "pal-button" >Palindrome</button><button class = "len-button" >Length</button><button class = "cv-button" >Count Vowels</button><button class = "rmv-button">Remove Vowels</button></div>'
+      const str = document.querySelector('.bottom');
+      str.innerHTML = butt;
+  } else if (method == "A&A") {
+    resetCalculator();
+    updateDisplay();
+    const butt = '<input type = "input" class = "is-input" placeholder = "#,#,#,#" id = "rev-input" value = ""></input><button class = "bub-button">Bubble Sort</button></div><button class = "ins-button" >Insertion Sort</button><button class = "sel-button" >Selection Sort</button><button class = "mer-button" >Merge Sort</button><button class = "qui-button">Quick Sort</button><button class = "hea-button">Heap Sort</button></div>'
+    const str = document.querySelector('.bottom');
     str.innerHTML = butt;
-  } else if (method == "Sorting") {
-    sorting(method);
   } else {
     /* Figure out what to do here */
    }
 }
-
-
-
 
 document.addEventListener("DOMContentLoaded",init());
 
